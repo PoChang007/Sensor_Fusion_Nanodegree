56b4f868f8ab80e0414d7cc37e06465b37dbffcd
PoChang-Su <pochang007@gmail.com>
2021-08-10 22:02:35 +0900

    added visualization of top-down view of Lidar data

--- Project_3_3D_Object_Tracking/src/FinalProject_Camera.cpp
+++ Project_3_3D_Object_Tracking/src/FinalProject_Camera.cpp
@@ -35,9 +35,9 @@ int main(int argc, const char *argv[])
     string imgPrefix = "KITTI/2011_09_26/image_02/data/000000"; // left camera, color
     string imgFileType = ".png";
     int imgStartIndex = 0; // first file index to load (assumes Lidar and camera names have identical naming convention)
-    int imgEndIndex = 10;   // last file index to load
-    int imgStepWidth = 1; 
-    int imgFillWidth = 4;  // no. of digits which make up the file index (e.g. img-0001.png)
+    int imgEndIndex = 10;  // last file index to load
+    int imgStepWidth = 1;
+    int imgFillWidth = 4; // no. of digits which make up the file index (e.g. img-0001.png)
 
     // object detection
     string yoloBasePath = dataPath + "dat/yolo/";
@@ -50,33 +50,66 @@ int main(int argc, const char *argv[])
     string lidarFileType = ".bin";
 
     // calibration data for camera and lidar
-    cv::Mat P_rect_00(3,4,cv::DataType<double>::type); // 3x4 projection matrix after rectification
-    cv::Mat R_rect_00(4,4,cv::DataType<double>::type); // 3x3 rectifying rotation to make image planes co-planar
-    cv::Mat RT(4,4,cv::DataType<double>::type); // rotation matrix and translation vector
-    
-    RT.at<double>(0,0) = 7.533745e-03; RT.at<double>(0,1) = -9.999714e-01; RT.at<double>(0,2) = -6.166020e-04; RT.at<double>(0,3) = -4.069766e-03;
-    RT.at<double>(1,0) = 1.480249e-02; RT.at<double>(1,1) = 7.280733e-04; RT.at<double>(1,2) = -9.998902e-01; RT.at<double>(1,3) = -7.631618e-02;
-    RT.at<double>(2,0) = 9.998621e-01; RT.at<double>(2,1) = 7.523790e-03; RT.at<double>(2,2) = 1.480755e-02; RT.at<double>(2,3) = -2.717806e-01;
-    RT.at<double>(3,0) = 0.0; RT.at<double>(3,1) = 0.0; RT.at<double>(3,2) = 0.0; RT.at<double>(3,3) = 1.0;
-    
-    R_rect_00.at<double>(0,0) = 9.999239e-01; R_rect_00.at<double>(0,1) = 9.837760e-03; R_rect_00.at<double>(0,2) = -7.445048e-03; R_rect_00.at<double>(0,3) = 0.0;
-    R_rect_00.at<double>(1,0) = -9.869795e-03; R_rect_00.at<double>(1,1) = 9.999421e-01; R_rect_00.at<double>(1,2) = -4.278459e-03; R_rect_00.at<double>(1,3) = 0.0;
-    R_rect_00.at<double>(2,0) = 7.402527e-03; R_rect_00.at<double>(2,1) = 4.351614e-03; R_rect_00.at<double>(2,2) = 9.999631e-01; R_rect_00.at<double>(2,3) = 0.0;
-    R_rect_00.at<double>(3,0) = 0; R_rect_00.at<double>(3,1) = 0; R_rect_00.at<double>(3,2) = 0; R_rect_00.at<double>(3,3) = 1;
-    
-    P_rect_00.at<double>(0,0) = 7.215377e+02; P_rect_00.at<double>(0,1) = 0.000000e+00; P_rect_00.at<double>(0,2) = 6.095593e+02; P_rect_00.at<double>(0,3) = 0.000000e+00;
-    P_rect_00.at<double>(1,0) = 0.000000e+00; P_rect_00.at<double>(1,1) = 7.215377e+02; P_rect_00.at<double>(1,2) = 1.728540e+02; P_rect_00.at<double>(1,3) = 0.000000e+00;
-    P_rect_00.at<double>(2,0) = 0.000000e+00; P_rect_00.at<double>(2,1) = 0.000000e+00; P_rect_00.at<double>(2,2) = 1.000000e+00; P_rect_00.at<double>(2,3) = 0.000000e+00;    
+    cv::Mat P_rect_00(3, 4, cv::DataType<double>::type); // 3x4 projection matrix after rectification
+    cv::Mat R_rect_00(4, 4, cv::DataType<double>::type); // 3x3 rectifying rotation to make image planes co-planar
+    cv::Mat RT(4, 4, cv::DataType<double>::type);        // rotation matrix and translation vector
+
+    RT.at<double>(0, 0) = 7.533745e-03;
+    RT.at<double>(0, 1) = -9.999714e-01;
+    RT.at<double>(0, 2) = -6.166020e-04;
+    RT.at<double>(0, 3) = -4.069766e-03;
+    RT.at<double>(1, 0) = 1.480249e-02;
+    RT.at<double>(1, 1) = 7.280733e-04;
+    RT.at<double>(1, 2) = -9.998902e-01;
+    RT.at<double>(1, 3) = -7.631618e-02;
+    RT.at<double>(2, 0) = 9.998621e-01;
+    RT.at<double>(2, 1) = 7.523790e-03;
+    RT.at<double>(2, 2) = 1.480755e-02;
+    RT.at<double>(2, 3) = -2.717806e-01;
+    RT.at<double>(3, 0) = 0.0;
+    RT.at<double>(3, 1) = 0.0;
+    RT.at<double>(3, 2) = 0.0;
+    RT.at<double>(3, 3) = 1.0;
+
+    R_rect_00.at<double>(0, 0) = 9.999239e-01;
+    R_rect_00.at<double>(0, 1) = 9.837760e-03;
+    R_rect_00.at<double>(0, 2) = -7.445048e-03;
+    R_rect_00.at<double>(0, 3) = 0.0;
+    R_rect_00.at<double>(1, 0) = -9.869795e-03;
+    R_rect_00.at<double>(1, 1) = 9.999421e-01;
+    R_rect_00.at<double>(1, 2) = -4.278459e-03;
+    R_rect_00.at<double>(1, 3) = 0.0;
+    R_rect_00.at<double>(2, 0) = 7.402527e-03;
+    R_rect_00.at<double>(2, 1) = 4.351614e-03;
+    R_rect_00.at<double>(2, 2) = 9.999631e-01;
+    R_rect_00.at<double>(2, 3) = 0.0;
+    R_rect_00.at<double>(3, 0) = 0;
+    R_rect_00.at<double>(3, 1) = 0;
+    R_rect_00.at<double>(3, 2) = 0;
+    R_rect_00.at<double>(3, 3) = 1;
+
+    P_rect_00.at<double>(0, 0) = 7.215377e+02;
+    P_rect_00.at<double>(0, 1) = 0.000000e+00;
+    P_rect_00.at<double>(0, 2) = 6.095593e+02;
+    P_rect_00.at<double>(0, 3) = 0.000000e+00;
+    P_rect_00.at<double>(1, 0) = 0.000000e+00;
+    P_rect_00.at<double>(1, 1) = 7.215377e+02;
+    P_rect_00.at<double>(1, 2) = 1.728540e+02;
+    P_rect_00.at<double>(1, 3) = 0.000000e+00;
+    P_rect_00.at<double>(2, 0) = 0.000000e+00;
+    P_rect_00.at<double>(2, 1) = 0.000000e+00;
+    P_rect_00.at<double>(2, 2) = 1.000000e+00;
+    P_rect_00.at<double>(2, 3) = 0.000000e+00;
 
     // misc
     double sensorFrameRate = 10.0 / imgStepWidth; // frames per second for Lidar and camera
-    int dataBufferSize = 2;       // no. of images which are held in memory (ring buffer) at the same time
-    vector<DataFrame> dataBuffer; // list of data frames which are held in memory at the same time
-    bool bVis = false;            // visualize results
+    int dataBufferSize = 2;                       // no. of images which are held in memory (ring buffer) at the same time
+    vector<DataFrame> dataBuffer;                 // list of data frames which are held in memory at the same time
+    bool bVis = false;                            // visualize results
 
     /* MAIN LOOP OVER ALL IMAGES */
 
-    for (size_t imgIndex = 0; imgIndex <= imgEndIndex - imgStartIndex; imgIndex+=imgStepWidth)
+    for (size_t imgIndex = 0; imgIndex <= imgEndIndex - imgStartIndex; imgIndex += imgStepWidth)
     {
         /* LOAD IMAGE INTO BUFFER */
         std::cout << "image index is " << imgIndex << "\n";
@@ -85,7 +118,7 @@ int main(int argc, const char *argv[])
         imgNumber << setfill('0') << setw(imgFillWidth) << imgStartIndex + imgIndex;
         string imgFullFilename = imgBasePath + imgPrefix + imgNumber.str() + imgFileType;
 
-        // load image from file 
+        // load image from file
         cv::Mat img = cv::imread(imgFullFilename);
 
         // push image into data frame buffer
@@ -95,16 +128,14 @@ int main(int argc, const char *argv[])
 
         cout << "#1 : LOAD IMAGE INTO BUFFER done" << endl;
 
-
         /* DETECT & CLASSIFY OBJECTS */
 
         float confThreshold = 0.2;
-        float nmsThreshold = 0.4;        
+        float nmsThreshold = 0.4;
         detectObjects((dataBuffer.end() - 1)->cameraImg, (dataBuffer.end() - 1)->boundingBoxes, confThreshold, nmsThreshold,
                       yoloBasePath, yoloClassesFile, yoloModelConfiguration, yoloModelWeights, true);
 
         cout << "#2 : DETECT & CLASSIFY OBJECTS done" << endl;
-
 
         /* CROP LIDAR POINTS */
 
@@ -112,33 +143,32 @@ int main(int argc, const char *argv[])
         string lidarFullFilename = imgBasePath + lidarPrefix + imgNumber.str() + lidarFileType;
         std::vector<LidarPoint> lidarPoints;
         loadLidarFromFile(lidarPoints, lidarFullFilename);
+        showLidarTopview(lidarPoints, cv::Size(4.0, 20.0), cv::Size(2000, 2000), true);
 
         // remove Lidar points based on distance properties
         float minZ = -1.5, maxZ = -0.9, minX = 2.0, maxX = 20.0, maxY = 2.0, minR = 0.1; // focus on ego lane
         cropLidarPoints(lidarPoints, minX, maxX, maxY, minZ, maxZ, minR);
-    
+
         (dataBuffer.end() - 1)->lidarPoints = lidarPoints;
 
         cout << "#3 : CROP LIDAR POINTS done" << endl;
-
 
         /* CLUSTER LIDAR POINT CLOUD */
 
         // associate Lidar points with camera-based ROI
         float shrinkFactor = 0.10; // shrinks each bounding box by the given percentage to avoid 3D object merging at the edges of an ROI
-        clusterLidarWithROI((dataBuffer.end()-1)->boundingBoxes, (dataBuffer.end() - 1)->lidarPoints, shrinkFactor, P_rect_00, R_rect_00, RT);
+        clusterLidarWithROI((dataBuffer.end() - 1)->boundingBoxes, (dataBuffer.end() - 1)->lidarPoints, shrinkFactor, P_rect_00, R_rect_00, RT);
 
         // Visualize 3D objects
         bVis = true;
-        if(bVis)
+        if (bVis)
         {
-            show3DObjects((dataBuffer.end()-1)->boundingBoxes, cv::Size(4.0, 20.0), cv::Size(2000, 2000), true);
+            show3DObjects((dataBuffer.end() - 1)->boundingBoxes, cv::Size(4.0, 20.0), cv::Size(2000, 2000), true);
         }
         bVis = false;
 
         cout << "#4 : CLUSTER LIDAR POINT CLOUD done" << endl;
-        
-        
+
         // REMOVE THIS LINE BEFORE PROCEEDING WITH THE FINAL PROJECT
         // continue; // skips directly to the next image without processing what comes beneath
 
@@ -146,10 +176,10 @@ int main(int argc, const char *argv[])
 
         // convert current image to grayscale
         cv::Mat imgGray;
-        cv::cvtColor((dataBuffer.end()-1)->cameraImg, imgGray, cv::COLOR_BGR2GRAY);
+        cv::cvtColor((dataBuffer.end() - 1)->cameraImg, imgGray, cv::COLOR_BGR2GRAY);
 
         // extract 2D keypoints from current image
-        vector<cv::KeyPoint> keypoints; // create empty feature list for current image
+        vector<cv::KeyPoint> keypoints;    // create empty feature list for current image
         string detectorType = "SHITOMASI"; // SHITOMASI, HARRIS, FAST, BRISK, ORB, AKAZE, SIFT
 
         double detectTime = 0.0;
@@ -185,7 +215,6 @@ int main(int argc, const char *argv[])
 
         cout << "#5 : DETECT KEYPOINTS done" << endl;
 
-
         /* EXTRACT KEYPOINT DESCRIPTORS */
 
         cv::Mat descriptors;
@@ -197,16 +226,15 @@ int main(int argc, const char *argv[])
 
         cout << "#6 : EXTRACT DESCRIPTORS done" << endl;
 
-
         if (dataBuffer.size() > 1) // wait until at least two images have been processed
         {
 
             /* MATCH KEYPOINT DESCRIPTORS */
 
             vector<cv::DMatch> matches;
-            string matcherType = "MAT_FLANN";        // MAT_BF, MAT_FLANN
+            string matcherType = "MAT_FLANN";  // MAT_BF, MAT_FLANN
             string descriptorType = "DES_HOG"; // DES_BINARY, DES_HOG
-            string selectorType = "SEL_KNN";       // SEL_NN, SEL_KNN
+            string selectorType = "SEL_KNN";   // SEL_NN, SEL_KNN
 
             matchDescriptors((dataBuffer.end() - 2)->keypoints, (dataBuffer.end() - 1)->keypoints,
                              (dataBuffer.end() - 2)->descriptors, (dataBuffer.end() - 1)->descriptors,
@@ -217,21 +245,20 @@ int main(int argc, const char *argv[])
 
             cout << "#7 : MATCH KEYPOINT DESCRIPTORS done" << endl;
 
-            
             /* TRACK 3D OBJECT BOUNDING BOXES */
 
             //// STUDENT ASSIGNMENT
             //// TASK FP.1 -> match list of 3D objects (vector<BoundingBox>) between current and previous frame (implement ->matchBoundingBoxes)
             map<int, int> bbBestMatches;
-            std::cout << "TASK FP.1"  << "\n";
-            matchBoundingBoxes(matches, bbBestMatches, *(dataBuffer.end()-2), *(dataBuffer.end()-1)); // associate bounding boxes between current and previous frame using keypoint matches
+            std::cout << "TASK FP.1"
+                      << "\n";
+            matchBoundingBoxes(matches, bbBestMatches, *(dataBuffer.end() - 2), *(dataBuffer.end() - 1)); // associate bounding boxes between current and previous frame using keypoint matches
             //// EOF STUDENT ASSIGNMENT
 
             // store matches in current data frame
-            (dataBuffer.end()-1)->bbMatches = bbBestMatches;
+            (dataBuffer.end() - 1)->bbMatches = bbBestMatches;
 
             cout << "#8 : TRACK 3D OBJECT BOUNDING BOXES done" << endl;
-
 
             /* COMPUTE TTC ON OBJECT IN FRONT */
 
@@ -258,11 +285,11 @@ int main(int argc, const char *argv[])
                 }
 
                 // compute TTC for current match
-                if( currBB->lidarPoints.size()>0 && prevBB->lidarPoints.size()>0 ) // only compute TTC if we have Lidar points
+                if (currBB->lidarPoints.size() > 0 && prevBB->lidarPoints.size() > 0) // only compute TTC if we have Lidar points
                 {
                     //// STUDENT ASSIGNMENT
                     //// TASK FP.2 -> compute time-to-collision based on Lidar data (implement -> computeTTCLidar)
-                    double ttcLidar; 
+                    double ttcLidar;
                     computeTTCLidar(prevBB->lidarPoints, currBB->lidarPoints, sensorFrameRate, ttcLidar);
                     //// EOF STUDENT ASSIGNMENT
 
@@ -270,7 +297,7 @@ int main(int argc, const char *argv[])
                     //// TASK FP.3 -> assign enclosed keypoint matches to bounding box (implement -> clusterKptMatchesWithROI)
                     //// TASK FP.4 -> compute time-to-collision based on camera (implement -> computeTTCCamera)
                     double ttcCamera;
-                    clusterKptMatchesWithROI(*currBB, (dataBuffer.end() - 2)->keypoints, (dataBuffer.end() - 1)->keypoints, (dataBuffer.end() - 1)->kptMatches);                    
+                    clusterKptMatchesWithROI(*currBB, (dataBuffer.end() - 2)->keypoints, (dataBuffer.end() - 1)->keypoints, (dataBuffer.end() - 1)->kptMatches);
                     computeTTCCamera((dataBuffer.end() - 2)->keypoints, (dataBuffer.end() - 1)->keypoints, currBB->kptMatches, sensorFrameRate, ttcCamera);
                     //// EOF STUDENT ASSIGNMENT
 
@@ -280,10 +307,10 @@ int main(int argc, const char *argv[])
                         cv::Mat visImg = (dataBuffer.end() - 1)->cameraImg.clone();
                         showLidarImgOverlay(visImg, currBB->lidarPoints, P_rect_00, R_rect_00, RT, &visImg);
                         cv::rectangle(visImg, cv::Point(currBB->roi.x, currBB->roi.y), cv::Point(currBB->roi.x + currBB->roi.width, currBB->roi.y + currBB->roi.height), cv::Scalar(0, 255, 0), 2);
-                        
+
                         char str[200];
                         sprintf(str, "TTC Lidarr : %f s, TTC Camera : %f s", (float)ttcLidar, ttcCamera);
-                        putText(visImg, str, cv::Point2f(80, 50), cv::FONT_HERSHEY_PLAIN, 2, cv::Scalar(0,0,255));
+                        putText(visImg, str, cv::Point2f(80, 50), cv::FONT_HERSHEY_PLAIN, 2, cv::Scalar(0, 0, 255));
 
                         string windowName = "Final Results : TTC";
                         cv::namedWindow(windowName, 4);
@@ -313,8 +340,7 @@ int main(int argc, const char *argv[])
                     bVis = false;
 
                 } // eof TTC computation
-            } // eof loop over all BB matches            
-
+            }     // eof loop over all BB matches
         }
 
     } // eof loop over all images
--- Project_3_3D_Object_Tracking/src/lidarData.cpp
+++ Project_3_3D_Object_Tracking/src/lidarData.cpp
@@ -5,53 +5,57 @@
 #include <opencv2/imgproc/imgproc.hpp>
 #include "lidarData.hpp"
 
-
 using namespace std;
 
 // remove Lidar points based on min. and max distance in X, Y and Z
 void cropLidarPoints(std::vector<LidarPoint> &lidarPoints, float minX, float maxX, float maxY, float minZ, float maxZ, float minR)
 {
-    std::vector<LidarPoint> newLidarPts; 
-    for(auto it=lidarPoints.begin(); it!=lidarPoints.end(); ++it) {
-        
-       if( (*it).x>=minX && (*it).x<=maxX && (*it).z>=minZ && (*it).z<=maxZ && (*it).z<=0.0 && abs((*it).y)<=maxY && (*it).r>=minR )  // Check if Lidar point is outside of boundaries
-       {
-           newLidarPts.push_back(*it);
-       }
+    std::vector<LidarPoint> newLidarPts;
+    for (auto it = lidarPoints.begin(); it != lidarPoints.end(); ++it)
+    {
+
+        if ((*it).x >= minX && (*it).x <= maxX && (*it).z >= minZ && (*it).z <= maxZ && (*it).z <= 0.0 && abs((*it).y) <= maxY && (*it).r >= minR) // Check if Lidar point is outside of boundaries
+        {
+            newLidarPts.push_back(*it);
+        }
     }
 
     lidarPoints = newLidarPts;
 }
-
-
 
 // Load Lidar points from a given location and store them in a vector
 void loadLidarFromFile(vector<LidarPoint> &lidarPoints, string filename)
 {
     // allocate 4 MB buffer (only ~130*4*4 KB are needed)
     unsigned long num = 1000000;
-    float *data = (float*)malloc(num*sizeof(float));
-    
+    float *data = (float *)malloc(num * sizeof(float));
+
     // pointers
-    float *px = data+0;
-    float *py = data+1;
-    float *pz = data+2;
-    float *pr = data+3;
-    
+    float *px = data + 0;
+    float *py = data + 1;
+    float *pz = data + 2;
+    float *pr = data + 3;
+
     // load point cloud
     FILE *stream;
-    stream = fopen (filename.c_str(),"rb");
-    num = fread(data,sizeof(float),num,stream)/4;
- 
-    for (int32_t i=0; i<num; i++) {
+    stream = fopen(filename.c_str(), "rb");
+    num = fread(data, sizeof(float), num, stream) / 4;
+
+    for (int32_t i = 0; i < num; i++)
+    {
         LidarPoint lpt;
-        lpt.x = *px; lpt.y = *py; lpt.z = *pz; lpt.r = *pr;
+        lpt.x = *px;
+        lpt.y = *py;
+        lpt.z = *pz;
+        lpt.r = *pr;
         lidarPoints.push_back(lpt);
-        px+=4; py+=4; pz+=4; pr+=4;
+        px += 4;
+        py += 4;
+        pz += 4;
+        pr += 4;
     }
     fclose(stream);
 }
-
 
 void showLidarTopview(std::vector<LidarPoint> &lidarPoints, cv::Size worldSize, cv::Size imageSize, bool bWait)
 {
@@ -67,7 +71,15 @@ void showLidarTopview(std::vector<LidarPoint> &lidarPoints, cv::Size worldSize, 
         int y = (-xw * imageSize.height / worldSize.height) + imageSize.height;
         int x = (-yw * imageSize.height / worldSize.height) + imageSize.width / 2;
 
-        cv::circle(topviewImg, cv::Point(x, y), 5, cv::Scalar(0, 0, 255), -1);
+        float zw = (*it).z;
+        if (zw > -1.40)
+        {
+            float val = it->x;
+            float maxVal = worldSize.height;
+            int red = min(255, (int)(255 * abs((val - maxVal) / maxVal)));
+            int green = min(255, (int)(255 * (1 - abs((val - maxVal) / maxVal))));
+            cv::circle(topviewImg, cv::Point(x, y), 5, cv::Scalar(0, green, red), -1);
+        }
     }
 
     // plot distance markers
@@ -82,8 +94,9 @@ void showLidarTopview(std::vector<LidarPoint> &lidarPoints, cv::Size worldSize, 
     // display image
     string windowName = "Top-View Perspective of LiDAR data";
     cv::namedWindow(windowName, 2);
+    cv::resize(topviewImg, topviewImg, cv::Size(960, 900));
     cv::imshow(windowName, topviewImg);
-    if(bWait)
+    if (bWait)
     {
         cv::waitKey(0); // wait for key to be pressed
     }
@@ -92,11 +105,12 @@ void showLidarTopview(std::vector<LidarPoint> &lidarPoints, cv::Size worldSize, 
 void showLidarImgOverlay(cv::Mat &img, std::vector<LidarPoint> &lidarPoints, cv::Mat &P_rect_xx, cv::Mat &R_rect_xx, cv::Mat &RT, cv::Mat *extVisImg)
 {
     // init image for visualization
-    cv::Mat visImg; 
-    if(extVisImg==nullptr)
+    cv::Mat visImg;
+    if (extVisImg == nullptr)
     {
         visImg = img.clone();
-    } else 
+    }
+    else
     {
         visImg = *extVisImg;
     }
@@ -104,31 +118,32 @@ void showLidarImgOverlay(cv::Mat &img, std::vector<LidarPoint> &lidarPoints, cv:
     cv::Mat overlay = visImg.clone();
 
     // find max. x-value
-    double maxVal = 0.0; 
-    for(auto it=lidarPoints.begin(); it!=lidarPoints.end(); ++it)
+    double maxVal = 0.0;
+    for (auto it = lidarPoints.begin(); it != lidarPoints.end(); ++it)
     {
-        maxVal = maxVal<it->x ? it->x : maxVal;
+        maxVal = maxVal < it->x ? it->x : maxVal;
     }
 
-    cv::Mat X(4,1,cv::DataType<double>::type);
-    cv::Mat Y(3,1,cv::DataType<double>::type);
-    for(auto it=lidarPoints.begin(); it!=lidarPoints.end(); ++it) {
+    cv::Mat X(4, 1, cv::DataType<double>::type);
+    cv::Mat Y(3, 1, cv::DataType<double>::type);
+    for (auto it = lidarPoints.begin(); it != lidarPoints.end(); ++it)
+    {
 
-            X.at<double>(0, 0) = it->x;
-            X.at<double>(1, 0) = it->y;
-            X.at<double>(2, 0) = it->z;
-            X.at<double>(3, 0) = 1;
+        X.at<double>(0, 0) = it->x;
+        X.at<double>(1, 0) = it->y;
+        X.at<double>(2, 0) = it->z;
+        X.at<double>(3, 0) = 1;
 
-            Y = P_rect_xx * R_rect_xx * RT * X;
-            cv::Point pt;
+        Y = P_rect_xx * R_rect_xx * RT * X;
+        cv::Point pt;
 
-            pt.x = Y.at<double>(0, 0) / Y.at<double>(2, 0); 
-            pt.y = Y.at<double>(1, 0) / Y.at<double>(2, 0); 
+        pt.x = Y.at<double>(0, 0) / Y.at<double>(2, 0);
+        pt.y = Y.at<double>(1, 0) / Y.at<double>(2, 0);
 
-            float val = it->x;
-            int red = min(255, (int)(255 * abs((val - maxVal) / maxVal)));
-            int green = min(255, (int)(255 * (1 - abs((val - maxVal) / maxVal))));
-            cv::circle(overlay, pt, 5, cv::Scalar(0, green, red), -1);
+        float val = it->x;
+        int red = min(255, (int)(255 * abs((val - maxVal) / maxVal)));
+        int green = min(255, (int)(255 * (1 - abs((val - maxVal) / maxVal))));
+        cv::circle(overlay, pt, 5, cv::Scalar(0, green, red), -1);
     }
 
     float opacity = 0.6;
@@ -138,8 +153,8 @@ void showLidarImgOverlay(cv::Mat &img, std::vector<LidarPoint> &lidarPoints, cv:
     if (extVisImg == nullptr)
     {
         string windowName = "LiDAR data on image overlay";
-        cv::namedWindow( windowName, 3 );
-        cv::imshow( windowName, visImg );
+        cv::namedWindow(windowName, 3);
+        cv::imshow(windowName, visImg);
         cv::waitKey(0); // wait for key to be pressed
     }
     else
